8cd951701cc19f80285d6e53fee2f86dce11573d
Hedron_JJenkins <jessetjenkins@gmail.com>
2017-05-24 14:59:35 -0700

    seriously updated this code, untested on RPi and mac

--- RFIDN2Cash.py
+++ RFIDN2Cash.py
@@ -5,18 +5,27 @@ Created on Thu May 18 12:02:22 2017
 
 @author: root
 """
+import sys
 import time
 import curses
-#import RPi.GPIO as GPIO #only works on Raspberry Pi
+import logging
 
-#GPIO.setmode(GPIO.BCM) #sets how the pins are labeled on the RPi
-#GPIO.setup(23,GPIO.OUT) #sets up pin 23 as an output
-#GPIO.set(23, GPIO.LOW) #initializes the pin to LOW. Laser off.
+logging.basicConfig(
+        level=logging.INFO,
+        # stream=sys.stdout,
+        filename='~/RFID2Cash/log.log',
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
-#dictionary stores RFID as keys and purchased tool time as values (minutes)
-credit_dict = { '0007421597': 0, '1512305969': 0,  '0007394263': 60, '0007406707': 0, '0007411862': 0, '0007395322': 0, '0007410979': 0, '0007416591':0.1, '0007398453':0}
+import RPi.GPIO as GPIO #only works on Raspberry Pi
 
-def Get_RFID():
+GPIO.setmode(GPIO.BCM) #sets how the pins are labeled on the RPi
+GPIO.setup(23,GPIO.OUT) #sets up pin 23 as an output
+GPIO.output(23, 0) #initializes the pin to LOW. Laser off.
+
+
+def get_RFID():
+    
     #returns RFID as a string
     #takes in nothing
     
@@ -40,59 +49,185 @@ def Get_RFID():
     
     return readtag
 
-def User_authorized(readtag):
+def user_authorized(readtag):
     #if user is authorized function returns 1
     #if user is not authorized (RFID TAG NOT IN DICTIONARY) returns 0
     
-    if readtag in credit_dict.keys():
-        print("User Authorized")
-        time.sleep(2)
+    if readtag in Credit_dict.keys():
+        logger.debug("User Authorized")
+        #time.sleep(2)
         return 1
     else:
-        print("Unauthorized User")
-        time.sleep(2)
+        logger.debug("Unauthorized User")
+        #time.sleep(2)
         return 0
         #break
         
-def Toggle_tool(state):
+#class Tool(object):
+#    def __init__(self, state=False):
+#        self.state = state
+#    
+#    def toggle(self):
+#        self.state = not self.state
+#
+#TOOL = Tool()
+
+def toggle_tool(state):
+# def toggle_tool(): should be lowercase
+#     TOOL.toggle()
+# state is a singleton (one in memory)
+# this should be something like tool.toggle() where tool.state is 0 or 1
+    
     #enables tool and returns 1 if state is 0
     #disables tool and returns 0 if state is 1
-    if state == 0:
+    if state == 1:
         print("enable tool")
         #enable tool command
-        #GPIO.set(23, GPIO.HIGH) 
+        GPIO.output(23, 1) 
         state = 1
         return state
-    elif state == 1:
+    elif state == 0:
         print("disable tool")
         #disable tool command
-        #GPIO.set(23, GPIO.LOW)
+        GPIO.output(23, 0)
         state = 0
         return state
     else:
         return print("State Not recognized")
 
+def user_credit(readtag):
+    #returns 0 if user is out of credit 
+    #returns 1 if user has credit
+    #takes in as an argument the RFID tag #
+
+    if Credit_dict[readtag] == 0:
+        return 0
+    elif Credit_dict[readtag] > 0:
+        return 1
+
+#def Display_user_credit(readtag):
+    #displays the user credit left on LCD screen
+    #takes in as an argument the RFID tag #
+    #insert code for LCD screen write, variable to use is credit_dict[readtag]
+
+def user_login(readtag):
+    #returns 1 if the user is logged in already
+    #returns 0 if the user is not logged in
+    #takes as an argument the RFID of the user.
+    if user_logged_in == 0:
+        return 0
+    else:
+        return 1
+
+def time_tracker(time):
+    #returns the time user has been using the machine.
+    #if argument is 1, timer starts and function returns start time in seconds
+    #if argument is 0, timer ends and elapsed time is returned in minutes 
+    
+    if time == 1:
+        global start
+        start = time.time() #time at beginning 
+        return start
+    if time == 0:
+        end = time.time() #at some other point
+        elapsed = (end - start) / 60
+        return elapsed
+
+def user_update(ID, usage):
+    #takes as an argument the time in minutes that the user used the laser
+    #returns nothing if successful
+    #returns 0 if user ran out of credit.
+    #updates a dictionary 
+    global Credit_dict
+    #store old user credit value
+    Current_credit = Credit_dict[ID] 
+    Credit_dict[ID] = Current_credit - usage
+    if Credit_dict[ID] <= 0:
+        return 0
+           
+#initialize global state variable to store machine state
+state = 0
+
+#initialize global variable to store time
+usage = 0
+
+#add dictionary that stores RFID as keys and corresponding purchased tool time as values (minutes)
+Credit_dict = { '0007421597': 0, '1512305969': 0,  '0007394263': 60, '0007406707': 0, '0007411862': 0, '0007395322': 0, '0007410979': 0, '0007416591':0.1, '0007398453':0}
+
+#initialize global variable to store user
+user_logged_in = 0
+
 try:
+    #initialize curses window functions
     stdscr = curses.initscr() 
     curses.noecho()
     curses.nocbreak()
-    #stdscr.keypad(1)
-    #initialize state variable
-    state = 0
-    #d = stdscr.getch()
+   
+    #start main loop
     while 1:
-        #get a reading from reader 
-        Current_readtag = Get_RFID()
-        if User_authorized(Current_readtag) == 1:
+        #need something to prevent other users from interrupting
+        #need something to cancel machine
+        
+        #get and store a reading from reader 
+        Current_readtag = get_RFID()
+        
+        
+        #conditional should check if RFID is in dictionary                            
+        if user_authorized(Current_readtag) == 1:
             #print to LCD
-            print("User authorized")
-            #enable the tool
-            Toggle_tool(0)
-        elif User_authorized(Current_readtag) == 0:
-            print("User unauthorized")
-        
-        
+            #print("User authorized") #print statement would be unnecessarily repeated
 
+            #if user is already logged in, store login time, update dict and turn off tool
+            if user_login(Current_readtag) == 1:
+                #turn off tool
+                toggle_tool(0)
+                #store elapsed time used tool and update dictionary
+                usage = time_tracker(0)
+                status = user_update(Current_readtag, usage)
+                if status == 0:
+                    print("User ran out of time")
+                    #print to LCD
+            
+            #executes if user is not already logged in    
+            elif user_login(Current_readtag) == 0:    #add "and user is not different from last logged in user" to this statement?
+                #return and store bool if user has credit or not
+                Credit_status = user_credit(Current_readtag)
+            
+                #store user credit as variable
+                Credit_remaining = Credit_dict[Current_readtag]
+            
+                if Credit_status == 0:
+                    #print to LCD
+                    #eventually use real username from django class
+                    print("User Out of Credit, Please Refill")
+                    #disable tool
+                    toggle_tool(0)
+                    
+                elif Credit_status == 1:
+                    #print to LCD
+                    print("Tool enabled, User Credit Remaining:  %d" + Credit_remaining)
+                    
+                    #update login variable
+                    user_logged_in = 1
+                   
+                    #turn machine on
+                    toggle_tool(1)
+                    
+                    #start tracking user time in seconds
+                    time_tracker(1)
+                        
+                    #tell user they are low on credit
+                    if Credit_remaining < 5:
+                            print("User credit is 5 minutes or less")
+                            
+                
+            
+        else:
+            print("Scan your RFID")
+        #clear RFID for next scan event
+        Current_readtag = 0
+        
+        
         
 finally:
     curses.nocbreak(); stdscr.keypad(0); curses.echo()
